f648b94a0e9743dab1774d3e9bdcce5aa597cd87
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2025-03-19 23:34:18 -0500

    Importacion no utilizada eliminada

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Linkedin, Phone } from "lucide-react";
-import { useState, useEffect } from "react";
 
 export default function TeamPage() {
 
